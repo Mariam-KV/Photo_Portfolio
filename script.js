295e023caa7920d190153022320855de34daed38
Mashawabupr <marikanik1999@gmail.com>
2022-09-23 18:21:07 +0300

    added spinner

---
+++ script.js
@@ -0,0 +1,9 @@
+let all = document.querySelector(".all");
+let spinner = document.querySelector(".spinner");
+
+all.style.display = "none";
+
+window.addEventListener("load", function () {
+  all.style.display = "block";
+  spinner.style.display = "none";
+});
